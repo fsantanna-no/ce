f3213e5821858e671b9624e6f227642cd539b9b4
fsantanna <francisco.santanna@gmail.com>
2020-04-22 23:37:48 -0300

    (+) data.c

---
+++ src/data.c
@@ -0,0 +1,40 @@
+#include "all.h"
+
+int data_isrec (Data data) {        // TODO: mutually recursive datas
+    int aux (Type type) {
+        switch (type.sub) {
+            case TYPE_UNIT:
+                return 0;
+            case TYPE_DATA:
+                if (!strcmp(data.tk.val.s,type.Data.tk.val.s)) {
+                    return 1;
+                }
+                return 0;
+            case TYPE_TUPLE:
+                for (int i=0; i<type.Tuple.size; i++) {
+                    if (aux(type.Tuple.vec[i])) {
+                        return 1;
+                    }
+                }
+                return 0;
+            default:
+                assert(0 && "TODO");
+        }
+    }
+    for (int i=0; i<data.size; i++) {
+        if (aux(data.vec[i].type)) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int datas_isrec (const char* data) {
+    for (int i=0; i<ALL.prog.size; i++) {
+        Glob g = ALL.prog.vec[i];
+        if (g.sub==GLOB_DATA && !strcmp(data,g.data.tk.val.s)) {
+            return data_isrec(g.data);
+        }
+    }
+    return 0;
+}
